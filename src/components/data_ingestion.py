c8d7938644e2fb38185763bc708fcd6ca4535a26
Aryan Dubey <aryandubey@Aryans-MacBook-Air.local>
2024-05-11 10:32:32 +0530

    Ingestion

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,50 @@
+import os
+import sys
+from src.exception import CustomException
+import pandas as pd
+from src.logger import logging
+
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path: str=os.path.join('artifacts',"train.csv")
+    test_data_path: str=os.path.join('artifacts',"test.csv")
+    row_data_path: str=os.path.join('artifacts',"data.csv")
+
+
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config=DataIngestionConfig
+
+    def initiate_data_ingestion(self):
+        logging.info("Entered data ingestion method or component")
+        try:
+         df=pd.read_csv('data/StudentsPerformance.csv')
+         logging.info('Reading the data ')
+         os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+         test.to_csv(self.ingestion_config.row_data_path,index=False,header=True)
+
+         logging.info('train Test seperation started')
+
+         train,test=train_test_split(df,test_size=0.2)
+
+         train.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+
+         test.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+
+         logging.info('Seperation of data completed')
+        
+         return(
+           self.ingestion_config.train_data_path,
+           self.ingestion_config.test_data_path
+         )
+
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+if __name__=="__main__":
+    obj=DataIngestion()
+    obj.initiate_data_ingestion()
