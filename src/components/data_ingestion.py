af2506420c3807876326b95093aabe56f7144382
Aryan Dubey <aryandubey@Aryans-MacBook-Air.local>
2024-05-12 00:46:22 +0530

    Model training

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -3,6 +3,7 @@ import sys
 from src.exception import CustomException
 import pandas as pd
 from src.logger import logging
+from src.components.model_trainer import ModelTrainer
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
@@ -51,4 +52,7 @@ if __name__=="__main__":
     train_data,test_data=obj.initiate_data_ingestion()
 
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transform(train_data,test_data)
+    train_arr,test_arr,_=data_transformation.initiate_data_transform(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
